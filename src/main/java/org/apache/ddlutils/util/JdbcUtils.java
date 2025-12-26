49eb14c4737559a40e90d9947d6f1217f349d785
vonlinee <vonlinee@163.com>
2025-12-26 23:08:46 +0800

    feat(JdbcUtils): add new connection methods and improve documentation

--- src/main/java/org/apache/ddlutils/util/JdbcUtils.java
+++ src/main/java/org/apache/ddlutils/util/JdbcUtils.java
@@ -5,16 +5,55 @@ import org.apache.ddlutils.platform.MetaDataColumnDescriptor;
 
 import javax.sql.DataSource;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public final class JdbcUtils {
 
   private JdbcUtils() {
   }
 
+  /**
+   * Creates a database connection from a DataSource.
+   *
+   * <p>
+   * This method attempts to create a database connection from the provided DataSource.
+   * If a username is provided (not null), it will use the username and password to establish the connection.
+   * If username is null, it will attempt to create a connection without authentication credentials.
+   * </p>
+   *
+   * <p>
+   * Example usage:
+   * </p>
+   *
+   * <pre>
+   * BasicDataSource dataSource = new BasicDataSource();
+   * dataSource.setUrl("jdbc:h2:~/test");
+   *
+   * // With username and password
+   * try {
+   *     Connection conn = JdbcUtils.getConnection(dataSource, "sa", "");
+   *     // Use the connection
+   *     conn.close();
+   * } catch (SQLException e) {
+   *     e.printStackTrace();
+   * }
+   *
+   * // Without username and password (using DataSource defaults)
+   * try {
+   *     Connection conn = JdbcUtils.getConnection(dataSource, null, null);
+   *     // Use the connection
+   *     conn.close();
+   * } catch (SQLException e) {
+   *     e.printStackTrace();
+   * }
+   * </pre>
+   *
+   * @param dataSource The DataSource to get the connection from
+   * @param username The database username (can be null)
+   * @param password The database password (can be null)
+   * @return A database connection
+   * @throws SQLException if a database access error occurs
+   */
   public static Connection getConnection(DataSource dataSource, String username, String password) throws SQLException {
     Connection connection;
     if (username != null) {
@@ -23,6 +62,80 @@ public final class JdbcUtils {
       connection = dataSource.getConnection();
     }
     return connection;
+  }
+
+  /**
+   * Creates a database connection using the provided properties.
+   *
+   * <p>
+   * This method expects the properties object to contain the following keys:
+   * </p>
+   * <ul>
+   * <li><code>url</code> - The database URL</li>
+   * <li><code>username</code> - The database username (optional)</li>
+   * <li><code>password</code> - The database password (optional)</li>
+   * </ul>
+   *
+   * <p>
+   * Example usage:
+   * </p>
+   *
+   * <pre>
+   * Properties props = new Properties();
+   * props.setProperty("url", "jdbc:h2:~/test");
+   * props.setProperty("username", "sa");
+   * props.setProperty("password", "");
+   *
+   * try {
+   *     Connection conn = JdbcUtils.getConnection(props);
+   *     // Use the connection
+   *     conn.close();
+   * } catch (SQLException e) {
+   *     e.printStackTrace();
+   * }
+   * </pre>
+   *
+   * @param properties The properties containing connection information
+   * @return A database connection
+   * @throws SQLException if a database access error occurs
+   */
+  public static Connection getConnection(Properties properties) throws SQLException {
+    String url = properties.getProperty("url");
+    String username = properties.getProperty("username");
+    String password = properties.getProperty("password");
+    return getConnection(url, username, password);
+  }
+
+  /**
+   * Creates a database connection using the provided URL, username, and password.
+   *
+   * <p>
+   * This method establishes a connection to a database using standard JDBC connection parameters.
+   * If username and password are null, the method will attempt to connect without authentication.
+   * </p>
+   *
+   * <p>
+   * Example usage:
+   * </p>
+   *
+   * <pre>
+   * try {
+   *     Connection conn = JdbcUtils.getConnection("jdbc:h2:~/test", "sa", "");
+   *     // Use the connection
+   *     conn.close();
+   * } catch (SQLException e) {
+   *     e.printStackTrace();
+   * }
+   * </pre>
+   *
+   * @param url The database URL (e.g., jdbc:h2:~/test)
+   * @param username The database username (can be null)
+   * @param password The database password (can be null)
+   * @return A database connection
+   * @throws SQLException if a database access error occurs
+   */
+  public static Connection getConnection(String url, String username, String password) throws SQLException {
+    return DriverManager.getConnection(url, username, password);
   }
 
   public static List<Map<String, Object>> toMapList(ResultSet rs) throws SQLException {
