a2c0b695751a0e9114449ae70371e0c194d3fb6a
vonlinee <vonlinee@163.com>
2025-12-27 16:16:13 +0800

    test(hsqldb): add test for column metadata assertion when NOT NULL is not declared explicitly.

--- src/test/java/org/apache/ddlutils/platform/hsqldb/HsqlDBPlatformTest.java
+++ src/test/java/org/apache/ddlutils/platform/hsqldb/HsqlDBPlatformTest.java
@@ -1,10 +1,13 @@
 package org.apache.ddlutils.platform.hsqldb;
 
+import org.apache.commons.collections4.map.CaseInsensitiveMap;
 import org.apache.ddlutils.util.JdbcUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 /**
@@ -25,4 +28,41 @@ public class HsqlDBPlatformTest {
       }
     }
   }
+
+  @Test
+  public void shouldCheckExplicitNotNullDeclaration() throws Exception {
+    try (Connection connection = DriverManager.getConnection("jdbc:hsqldb:mem:test", "sa", "")) {
+      try (Statement statement = connection.createStatement()) {
+        try {
+          statement.execute("CREATE TABLE ROUNDTRIP\n" +
+                            "(\n" +
+                            "    AVALUE INTEGER,\n" +
+                            "    pk1 INTEGER NOT NULL,\n" +
+                            "    pk2 VARCHAR(32) NOT NULL,\n" +
+                            "    pk3 DOUBLE DEFAULT 2,\n" +
+                            "    PRIMARY KEY (pk1, pk2, pk3)\n" +
+                            ");");
+          ResultSet rs = connection.getMetaData().getColumns(null, null,
+            "ROUNDTRIP", null);
+
+          CaseInsensitiveMap<String, String> values = new CaseInsensitiveMap<>();
+          while (rs.next()) {
+            String isNullable = rs.getString("IS_NULLABLE");
+            String columnName = rs.getString("COLUMN_NAME");
+            values.put(columnName, isNullable);
+            System.out.println(columnName + " " + isNullable);
+          }
+          Assert.assertEquals("YES", values.get("AVALUE"));
+          Assert.assertEquals("NO", values.get("PK1"));
+          Assert.assertEquals("NO", values.get("PK2"));
+
+          // in hsqldb 1.8.0.4, it's NO, in hsqldb 2.4.1, it's YES
+          Assert.assertEquals("YES", values.get("PK3"));
+        } catch (Throwable throwable) {
+          Assert.assertTrue(throwable.getMessage().contains("length must be specified in type definition: VARCHAR"));
+        }
+      }
+    }
+  }
+
 }
